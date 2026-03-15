dcc4816232278c47f045930b3d5196a3417d4226
issixe <isabel.liu747@gmail.com>
2026-03-14 23:29:43 -0400

    Add custom dataset loader

---
+++ data/politune_dataset.py
@@ -0,0 +1,25 @@
+from torchtune.data import Message
+from typing import Any, Mapping
+
+
+class PlainTextToMessages:
+    def __call__(self, sample: Mapping[str, Any]) -> Mapping[str, Any]:
+        chosen_messages = [
+            Message(role="user", content=sample["prompt"], masked=True, eot=True),
+            Message(role="assistant", content=sample["chosen"], masked=False, eot=True),
+        ]
+        rejected_messages = [
+            Message(role="user", content=sample["prompt"], masked=True, eot=True),
+            Message(role="assistant", content=sample["rejected"], masked=False, eot=True),
+        ]
+        return {"chosen": chosen_messages, "rejected": rejected_messages}
+
+
+def politune_dataset(tokenizer, source, split="train", **kwargs):
+    from torchtune.datasets._preference import PreferenceDataset
+    return PreferenceDataset(
+        tokenizer=tokenizer,
+        source=source,
+        message_transform=PlainTextToMessages(),
+        split=split,
+    )
